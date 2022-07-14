239f1525a2540b4670276f4646a039e64fe4872d
“Alexey“ <nvmqwer593@gmail.com>
2022-07-14 11:53:11 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix=[]) {
+  const arr = [];
+  let odd = false;
+  for (let subArr of matrix) {
+    if (odd) subArr = subArr.reverse();
+    for (let el of subArr) {
+      arr.push(el);
+    }
+    odd = !odd;
+  }
+  return arr;
 }
